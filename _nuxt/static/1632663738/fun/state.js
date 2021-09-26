bb028473213f109f3d2bc871a09578829876cc8f
ransewhale <ransewhale@users.noreply.github.com>
2021-09-26 13:49:09 +0000

    deploy: a58c509a53d35b8b12f4ebe00491dee7c045d9b8

--- _nuxt/static/1632663738/fun/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632663738",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffun",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"95a591f7"}}}}(null));
---
+++ _nuxt/static/1632664143/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fen\u002Fcontact","\u002Fko\u002Fcontact","\u002Fcontact","\u002Ffun","\u002Finspire","\u002Fprofile","\u002Fupdates","\u002Fen\u002Ffun","\u002Fen\u002Finspire","\u002Fen\u002Fprofile","\u002Fen\u002Fupdates","\u002Fko\u002Ffun","\u002Fko\u002Finspire","\u002Fko\u002Fprofile","\u002Fko\u002Fupdates","\u002Fen","\u002Fko"]})

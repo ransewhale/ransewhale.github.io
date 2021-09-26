07bbd7e39d4e3accecd6be53214164d8fca9a2b8
ransewhale <ransewhale@users.noreply.github.com>
2021-09-26 13:42:25 +0000

    deploy: 016271b539b5250b319e5d0a21b89c5532b8a613

---
+++ _nuxt/static/1632663738/ko/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632663738",layout:"default",error:a,serverRendered:true,routePath:"\u002Fko",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"95a591f7"}}}}(null));

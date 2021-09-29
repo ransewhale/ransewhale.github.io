9ba00a891b174ae4e3099a32adc073adc251c600
ransewhale <5298807+ransewhale@users.noreply.github.com>
2021-09-30 02:13:51 +0900

    アイコンをアニメ化

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <v-col cols="12" md="8">
         <v-card class="pa-4">
           <v-card-title>{{ $t('welcome') }}</v-card-title>
-          <v-img :src="require('@/assets/images/rw.svg')" />
+          <v-img :src="require('@/assets/images/rwanime.svg')" />
         </v-card>
         <v-row>
           <v-col cols="12" md="6">
@@ -25,6 +25,7 @@
           </v-col>
           <v-col cols="12" md="6">
             <v-card class="mt-4 pa-4" max-height="800">
+              <v-card-title>Lorem ipsum</v-card-title>
               <v-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
             </v-card>
           </v-col>

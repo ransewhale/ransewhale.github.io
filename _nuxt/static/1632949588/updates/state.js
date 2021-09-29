a0869ec76a25fef3ce1c0a57b67f0fa28176a149
ransewhale <ransewhale@users.noreply.github.com>
2021-09-29 21:06:34 +0000

    deploy: a6e0d568998a8c3af01e55148019d7dadcdb9ebd

---
+++ _nuxt/static/1632949588/updates/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632949588",layout:"default",error:a,serverRendered:true,routePath:"\u002Fupdates",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ab31431d"}}}}(null));

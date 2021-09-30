a0575742555a793ab15af70bd0fa0737cb102b0b
ransewhale <ransewhale@users.noreply.github.com>
2021-09-30 10:24:15 +0000

    deploy: 3943e870339337f7a3e14d8f5737c75d0d6d6ee2

--- _nuxt/static/1632951463/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1632951463",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e534aa58"}}}}(null,"\u002F"));
